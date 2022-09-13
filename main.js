fc85fa1af6a69db4f04dea22b8eda6dc5f9f8398
David Sword <swo17002@byui.edu>
2022-09-12 19:02:08 -0700

    Updated index html to be dynamic with links.

---
+++ main.js
@@ -0,0 +1,18 @@
+const links = [
+    {
+        label: "Week 1 Notes",
+        link: "week1/index.html"
+    }
+]
+var list_container = document.getElementById("list-container");
+for (let i = 0; i < links.length; i++){
+    var ilink = links[i].link;
+    var ilabel = links[i].label;
+    var list_object = document.createElement("li");
+    list_object.classList.add("white", "center");
+    var hyper_object = document.createElement("a");
+    hyper_object.innerText = ilabel;
+    hyper_object.href = ilink;
+    list_object.append(hyper_object);
+    list_container.append(list_object);
+}
