d14ccf26ed1aef706fd7b7e20f411bbf0206fd8a
David Sword <swo17002@byui.edu>
2022-09-26 15:34:17 -0700

    Chapter 14 Code Edits

--- W3-14-2-Code/main.js
+++ W3-14-2-Code/main.js
@@ -1,8 +1,20 @@
 function byTagName(node, tagName){
-    let i = [];
+  let x = [];
+  let tagNameUpper = tagName.toUpperCase()
     for(let i = 0; i < node.childNodes.length; i++){
-      console.log(node[i]);
+      let curr = node.childNodes[i];
+      if(curr.tagName === tagNameUpper){
+        x.push(curr);
+      }
+      if(curr.childNodes.length > 0){
+        for(let j = 0; j < curr.childNodes.length; j++){
+          if(curr.childNodes[j].tagName === tagNameUpper){
+            x.push(curr.childNodes[j])
+          }
+        }
+      }
     }
+    return(x)
 }
 
 console.log(byTagName(document.body, "h1").length);
