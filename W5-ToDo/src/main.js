224433ca3549edd9ec4a10df066846ae403091fc
David Sword <swo17002@byui.edu>
2022-10-12 19:08:19 -0700

    Added controls for click and tap

--- W5-ToDo/src/main.js
+++ W5-ToDo/src/main.js
@@ -1,5 +1,27 @@
 let submit_buttons = document.getElementsByClassName("submit");
 
-for(let button in submit_buttons){
-    console.log(button.parentNode);
+
+
+for(let button of submit_buttons){
+    button.parentElement.id = Date.now();
+    button.addEventListener("touchstart", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        if(button.parentElement.classList.contains("completed")){
+            button.parentElement.classList.remove("completed")
+        } else {
+            button.parentElement.classList.add("completed")
+        }
+        console.log(Date.now());
+    }, false);
+    button.addEventListener("click", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        if(button.parentElement.classList.contains("completed")){
+            button.parentElement.classList.remove("completed")
+        } else {
+            button.parentElement.classList.add("completed")
+        }
+        console.log(Date.now());
+    }, false);
 }
