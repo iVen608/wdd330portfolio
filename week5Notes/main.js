834e52deb65cc7cf2be280cb42fcb950ef726089
David Sword <swo17002@byui.edu>
2022-10-16 10:58:51 -0700

    Changed how notes are inputted

--- week5Notes/main.js
+++ week5Notes/main.js
@@ -1,3 +1,19 @@
-import notes from "../notes/week5notes.js";
+import notes from "../notes/week5notesArray.js";
 
-document.getElementById("output").innerHTML = notes;
+notes.forEach((note)=>{
+    if(note.substring(0,1) === "!"){
+        const h3 = document.createElement("h3");
+        h3.innerHTML = note.substring(1, note.length);
+        document.getElementById("output").appendChild(h3);
+    } else {
+        const li = document.createElement("li");
+        li.innerHTML = note;
+        if((note.substring(0,1) === "#")){
+            
+        }
+        
+        document.getElementById("output").appendChild(li);
+    }
+})
+ 
+//document.getElementById("output").innerHTML = notes;
