900003b968d3b42400c12bb5c1c716f8460b5619
David Sword <swo17002@byui.edu>
2022-11-04 20:23:14 -0700

    W7 Notes

--- src/notesCompiler.js
+++ src/notesCompiler.js
@@ -3,14 +3,14 @@ export default class notesCompiler {
         notes.forEach((note)=>{
             if(note.substring(0,1) === "!"){
                 const h3 = document.createElement("h3");
-                h3.innerHTML = note.substring(1, note.length);
+                h3.textContent = note.substring(1, note.length);
                 output.appendChild(h3);
             } else {
                 const li = document.createElement("li");
-                li.innerHTML = note;
+                li.textContent = note;
                 if((note.substring(0,1) === "#")){
                     li.classList.add("code");
-                    li.innerHTML = note.substring(1, note.length);
+                    li.textContent = note.substring(1, note.length);
                 }
                 output.appendChild(li);
             }
